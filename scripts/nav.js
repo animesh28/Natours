8117e0412ddf4cef5cb811455e2c824dc406aed1
animesh28 <animesh.raj.om@gmail.com>
2021-05-17 15:17:40 +0530

    nav js added

---
+++ scripts/nav.js
@@ -0,0 +1,20 @@
+document.querySelector('.navigation__link--1').onclick = function() {
+    var checkbox = document.querySelector('#navi-toggle');
+        checkbox.checked = !checkbox.checked;
+}
+document.querySelector('.navigation__link--2').onclick = function() {
+    var checkbox = document.querySelector('#navi-toggle');
+        checkbox.checked = !checkbox.checked;
+}
+document.querySelector('.navigation__link--3').onclick = function() {
+    var checkbox = document.querySelector('#navi-toggle');
+        checkbox.checked = !checkbox.checked;
+}
+document.querySelector('.navigation__link--4').onclick = function() {
+    var checkbox = document.querySelector('#navi-toggle');
+        checkbox.checked = !checkbox.checked;
+}
+document.querySelector('.navigation__link--5').onclick = function() {
+    var checkbox = document.querySelector('#navi-toggle');
+        checkbox.checked = !checkbox.checked;
+}
